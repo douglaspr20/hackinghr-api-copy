9611b36cac57efdf5285ed2ec14529eb95a04ffe
love1119 <goldenstar007.dev@gmail.com>
2020-12-28 02:00:12 -0800

    set port

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 3000;
 const db = require("./models/index.js");
 
 const bodyParser = require("body-parser");
@@ -12,7 +11,7 @@ app.post("/url", function (req, res) {
   const url = req.body.url;
   console.log('******* req.body', req.body);
   console.log('******* req.query', req.query);
-
+  
   db.Url.findOrCreate({ where: { url: url } }).then(([urlObj, created]) => {
     console.log("******** urlObj", urlObj);
     console.log("******** created", created);
@@ -22,8 +21,10 @@ app.post("/url", function (req, res) {
 
 app.get("/url", function (req, res) {
   const url = req.body.url;
-
+  
   res.send("******  success  ******");
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
