ee3f8cdd763d7032afbb35867cdd1eba336adc7b
love1119 <goldenstar007.dev@gmail.com>
2020-12-29 00:44:23 -0800

    test log

--- models/index.js
+++ models/index.js
@@ -9,7 +9,11 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+console.log('********* config', config);
+console.log('********* process.env', process.env);
+console.log('********* __dirname', __dirname);
 if (config.use_env_variable) {
+  console.log('********* env variable', process.env[config.use_env_variable]);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
