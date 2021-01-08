d8da7115124e060ce337eb3fa469c1e66e696eb0
love1119 <goldenstar007.dev@gmail.com>
2021-01-08 08:44:32 -0800

    built upgrade plan API

--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -1,6 +1,7 @@
 const privateRoutes = {
   "GET /user/": "UserController.getUser",
   "PUT /user/": "UserController.updateUser",
+  "PUT /user/upgrade-plan/": "UserController.upgradePlan",
 };
 
 module.exports = privateRoutes;
