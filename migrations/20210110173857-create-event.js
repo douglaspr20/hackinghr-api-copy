cf5551fb4a3275ec77523b6fc2237667caee4a7b
love1119 <goldenstar007.dev@gmail.com>
2021-01-10 09:50:50 -0800

    create event model

---
+++ migrations/20210110173857-create-event.js
@@ -0,0 +1,46 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Events", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      organizer: Sequelize.STRING,
+      date: Sequelize.STRING,
+      time: Sequelize.STRING,
+      timezone: Sequelize.STRING,
+      category: Sequelize.STRING,
+      ticket: {
+        type: Sequelize.STRING,
+        defaultValue: "free",
+        values: ["free", "priced"],
+      },
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      location: Sequelize.ARRAY(Sequelize.STRING),
+      description: Sequelize.TEXT,
+      link: Sequelize.STRING,
+      credit: Sequelize.JSON,
+      code: Sequelize.STRING,
+      image: Sequelize.STRING,
+      image2: Sequelize.STRING,
+      status: Sequelize.STRING,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Events");
+  },
+};
