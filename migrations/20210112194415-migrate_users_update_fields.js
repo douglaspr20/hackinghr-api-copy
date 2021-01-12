163ac8ebde32ec0510b7571cac39cc970b418f81
love1119 <goldenstar007.dev@gmail.com>
2021-01-12 11:57:08 -0800

    add location and city to users model

---
+++ migrations/20210112194415-migrate_users_update_fields.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn("Users", "location", {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
+      queryInterface.addColumn("Users", "city", {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
