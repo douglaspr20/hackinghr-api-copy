26a89a4d55c7566f9c9b33532dd2301a4ab03b58
love1119 <goldenstar007.dev@gmail.com>
2021-01-14 04:37:25 -0800

    add users and status field to events model

--- models/event.js
+++ models/event.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
     /**
@@ -12,31 +10,41 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Event.init({
-    title: DataTypes.STRING,
-    organizer: DataTypes.STRING,
-    startDate: DataTypes.STRING,
-    endDate: DataTypes.STRING,
-    timezone: DataTypes.STRING,
-    category: DataTypes.STRING,
-    ticket: {
-      type: DataTypes.STRING,
-      defaultValue: "free",
-      values: ["free", "priced"],
+  }
+  Event.init(
+    {
+      title: DataTypes.STRING,
+      organizer: DataTypes.STRING,
+      startDate: DataTypes.STRING,
+      endDate: DataTypes.STRING,
+      timezone: DataTypes.STRING,
+      category: DataTypes.STRING,
+      ticket: {
+        type: DataTypes.STRING,
+        defaultValue: "free",
+        values: ["free", "priced"],
+      },
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      location: DataTypes.ARRAY(DataTypes.STRING),
+      description: DataTypes.JSON,
+      link: DataTypes.STRING,
+      credit: DataTypes.JSON,
+      code: DataTypes.STRING,
+      image: DataTypes.STRING,
+      image2: DataTypes.STRING,
+      status: {
+        type: DataTypes.JSON,
+        defaultValue: {},
+      },
+      users: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        defaultValue: [],
+      },
     },
-    type: DataTypes.ARRAY(DataTypes.STRING),
-    location: DataTypes.ARRAY(DataTypes.STRING),
-    description: DataTypes.JSON,
-    link: DataTypes.STRING,
-    credit: DataTypes.JSON,
-    code: DataTypes.STRING,
-    image: DataTypes.STRING,
-    image2: DataTypes.STRING,
-    status: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'Event',
-  });
+    {
+      sequelize,
+      modelName: "Event",
+    }
+  );
   return Event;
 };
