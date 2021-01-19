efecd2eb2f5896438d023a0463415935798a79f9
love1119 <goldenstar007.dev@gmail.com>
2021-01-19 10:25:22 -0800

    create mentoring table

---
+++ migrations/20210119182025-create-mentoring.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Mentorings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      about: {
+        type: Sequelize.TEXT,
+      },
+      areas: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        defaultValue: [],
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Mentorings');
+  }
+};
---
+++ models/mentoring.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Mentoring extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Mentoring.init({
+    title: DataTypes.STRING,
+    about: DataTypes.TEXT,
+    areas: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      defaultValue: [],
+    }
+  }, {
+    sequelize,
+    modelName: 'Mentoring',
+  });
+  return Mentoring;
+};
