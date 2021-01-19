ff7a7ca14b38300ddcc7ebb9be729379797800bc
love1119 <goldenstar007.dev@gmail.com>
2021-01-19 10:35:29 -0800

    add 2 fields to mentoring table

--- models/mentoring.js
+++ models/mentoring.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Mentoring extends Model {
     /**
@@ -12,17 +10,28 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Mentoring.init({
-    title: DataTypes.STRING,
-    about: DataTypes.TEXT,
-    areas: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      defaultValue: [],
+  }
+  Mentoring.init(
+    {
+      title: DataTypes.STRING,
+      about: DataTypes.TEXT,
+      areas: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        defaultValue: [],
+      },
+      isMentor: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false, // true: mentor; false: mentee
+      },
+      connectedMembers: {
+        type: DataTypes.ARRAY(DataTypes.INTEGER),
+        defaultValue: [],
+      },
+    },
+    {
+      sequelize,
+      modelName: "Mentoring",
     }
-  }, {
-    sequelize,
-    modelName: 'Mentoring',
-  });
+  );
   return Mentoring;
 };
