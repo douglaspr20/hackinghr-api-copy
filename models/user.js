00e37de99a2171136314f229ad75dec15edacae1
love1119 <goldenstar007.dev@gmail.com>
2021-02-08 17:37:09 -0800

    save data when user clicks on attend

--- models/user.js
+++ models/user.js
@@ -54,6 +54,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.INTEGER,
         defaultValue: 0,
       },
+      attended: {
+        type: DataTypes.JSON,
+        defaultValue: {},
+      },
     },
     {
       sequelize,
