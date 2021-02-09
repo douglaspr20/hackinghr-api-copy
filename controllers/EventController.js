039b64b33c4654fb7c7459dca88068fa14a84a91
love1119 <goldenstar007.dev@gmail.com>
2021-02-08 18:26:12 -0800

    revert sort by name

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -318,8 +318,7 @@ const EventController = () => {
           SELECT public."Events".id as eventId, public."Users".*
           FROM public."Events"
           JOIN public."Users" ON public."Users".id = ANY (public."Events".users::int[])
-          WHERE public."Events".id = ${id}
-          ORDER BY public."Users".firstName;
+          WHERE public."Events".id = ${id};
         `;
 
         const userList = await db.sequelize.query(query, {
