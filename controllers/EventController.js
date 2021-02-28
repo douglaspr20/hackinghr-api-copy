b7350e8ecefe7fc62302df8c0887bd228f4ac1d4
love1119 <goldenstar007.dev@gmail.com>
2021-02-28 09:37:52 -0800

    add log for event reminders

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -19,8 +19,11 @@ const EventController = () => {
     const interval1 = `0 ${dateBefore24Hours.minutes()} ${dateBefore24Hours.hours()} ${dateBefore24Hours.date()} ${dateBefore24Hours.month()} *`;
     const dateBefore2Hours = moment(event.startDate).subtract(45, "minutes");
     const interval2 = `0 ${dateBefore2Hours.minutes()} ${dateBefore2Hours.hours()} ${dateBefore2Hours.date()} ${dateBefore2Hours.month()} *`;
+    
+    console.log('****** set event reminders ', interval1, interval2)
 
     cronService().addTask(`${event.title}-24`, interval1, true, async () => {
+      console.log('********* event 24 hours later', event)
       const targetEvent = await Event.findOne({ where: { id: event.id } });
       const eventUsers = await Promise.all(
         (targetEvent.users || []).map((user) => {
@@ -59,7 +62,8 @@ const EventController = () => {
       );
     });
 
-    cronService().addTask(`${event.title}-2`, interval2, true, async () => {
+    cronService().addTask(`${event.title}-45`, interval2, true, async () => {
+      console.log('********* event 45 minutes hours later', event)
       const targetEvent = await Event.findOne({ where: { id: event.id } });
       const eventUsers = await Promise.all(
         (targetEvent.users || []).map((user) => {
