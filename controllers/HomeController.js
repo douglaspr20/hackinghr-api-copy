b382f69ad148e652fb83e36a5cdb2de78aeb3f4a
Juan Camilo Martinez Morales <xogost@gmail.com>
2021-03-18 20:34:12 -0500

    The topics user were added in the recommendations algorithm

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,7 +1,6 @@
 const db = require("../models");
 const HttpCodes = require("http-codes");
-const { literal, Op, fn } = require("sequelize");
-const SortOptions = require("../enum/FilterSettings").SORT_OPTIONS;
+const { Op, fn } = require("sequelize");
 
 const Library = db.Library;
 const Podcast = db.Podcast;
@@ -9,50 +8,60 @@ const Event = db.Event;
 const ConferenceLibrary = db.ConferenceLibrary;
 
 const HomeController = () => {
+  /**
+   * New algorithm to get recommendations.
+   * @param {*} req 
+   * @param {*} res 
+   * @returns Json object with all recommendations.
+   */
   const getRecommendations = async (req, res) => {
+    const { user } = req;
+
     try {
-      const libraries = await Library.findAll({
-        where: {
+      let order = [ [fn('RANDOM')] ];
+      let limit = 3;
+      let where = {
+        libraries: {
           approvalStatus: 'approved',
         },
-        order: [
-          [fn('RANDOM')]
-        ],
-        limit: 3,
-      });
-
-      const podcasts = await Podcast.findAll({
-        order: [
-          [fn('RANDOM')]
-        ],
-        limit: 3,
-      });
-
-      const events = await Event.findAll({
-        where: {
+        podcast: {},
+        events: {
           startDate: {
             [Op.gte]: new Date(),
           }
-
         },
-        order: [
-          [fn('RANDOM')]
-        ],
-        limit: 3,
+        conference: {},
+      };
+
+      if(user.topicsOfInterest != null && user.topicsOfInterest.length > 0){
+        where["libraries"]["topics"] = { [Op.overlap]: user.topicsOfInterest };
+        where["podcast"]["topics"] = { [Op.overlap]: user.topicsOfInterest };
+        where["events"]["categories"] = { [Op.overlap]: user.topicsOfInterest };
+        where["conference"]["categories"] = { [Op.overlap]: user.topicsOfInterest };
+      }
+      const libraries = await Library.findAll({
+        where: where["libraries"],
+        order,
+        limit,
+      });
+
+      const podcasts = await Podcast.findAll({
+        where: where["podcast"],
+        order,
+        limit,
+      });
+
+      const events = await Event.findAll({
+        where: where["events"],
+        order,
+        limit,
       });
 
       const conferenceLibrary = await ConferenceLibrary.findAll({
-        order: [
-          [fn('RANDOM')]
-        ],
-        limit: 3,
+        where: where["conference"],
+        order,
+        limit,
       });
-
-      if (!libraries) {
-        return res
-          .status(HttpCodes.INTERNAL_SERVER_ERROR)
-          .json({ msg: "Bad Request: Recommendations not found" });
-      }
 
       return res.status(HttpCodes.OK).json({
         libraries,
