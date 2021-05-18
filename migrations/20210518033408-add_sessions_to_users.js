901b72724bfccfe92586e8c76b751d5f92e8601b
love1119 <goldenstar007.dev@gmail.com>
2021-05-17 21:48:29 -0700

    add session

---
+++ migrations/20210518033408-add_sessions_to_users.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn("Users", "sessions", {
+        type: Sequelize.ARRAY(Sequelize.INTEGER),
+        defaultValue: [],
+      }),
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
