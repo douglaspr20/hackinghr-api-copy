225d4229b41932e237f3413c439e2f9d6f8f9e37
lourencelinao13 <lourencelinao13@gmail.com>
2021-10-27 01:17:01 +0800

    [ADJUSTMENT][Lou] - Skill Cohort

--- app.js
+++ app.js
@@ -179,8 +179,7 @@ cron.schedule(
 
 // cron job that notifies a cohort participants that a resource for the day is available through notification and email
 cron.schedule(
-  // "0 2 * * *", // 2AM everyday
-  "*/2 * * * *",
+  "0 2 * * *", // 2AM everyday
   async () => {
     console.log("running a task every 2 AM.");
     console.log("****************Notification****************");
