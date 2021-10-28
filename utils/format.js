0920b85523c4023e72c359a70f5e40b51cba35d3
lourencelinao13 <lourencelinao13@gmail.com>
2021-10-28 21:54:38 +0800

    [FIX][Lou] - Events

--- utils/format.js
+++ utils/format.js
@@ -58,14 +58,8 @@ function getEventPeriod(date, startAndEndTimes, timezone) {
 
   return startAndEndTimes.map((time, index) => {
     console.log(time.startTime, time.endTime);
-    let startTime = convertToCertainTime(
-      moment(time.startTime).utcOffset(tz.offset, true),
-      timezone
-    );
-    let endTime = convertToCertainTime(
-      moment(time.endTime).utcOffset(tz.offset, true),
-      timezone
-    );
+    let startTime = convertToCertainTime(moment(time.startTime), timezone);
+    let endTime = convertToCertainTime(moment(time.endTime), timezone);
 
     console.log(moment(startTime), moment(endTime));
     console.log(`
