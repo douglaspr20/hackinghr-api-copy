2d96ab6fb73a1d49d21b3562fc971f34191f3158
Douglas Pérez <douglas.eduardo2000@gmail.com>
2021-11-18 09:48:16 -0400

    default value migration

--- migrations/20211117221405-added-column-added-first-session.js
+++ migrations/20211117221405-added-column-added-first-session.js
@@ -10,7 +10,7 @@ module.exports = {
      */
     await queryInterface.addColumn("Users", "addedFirstSession", {
       type: Sequelize.BOOLEAN,
-      defaultValue: 0,
+      defaultValue: false,
     });
   },
 
