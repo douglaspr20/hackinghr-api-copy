20ba123b16a7c5e458d7890a49d757b37d69aad7
lourencelinao13 <lourencelinao13@gmail.com>
2022-01-18 01:40:16 +0800

    Weekly Digest

--- controllers/WeeklyDigestController.js
+++ controllers/WeeklyDigestController.js
@@ -4,9 +4,53 @@ const s3Service = require("../services/s3.service");
 const moment = require("moment-timezone");
 
 const Podcast = db.Podcast;
+const Library = db.Library;
 
 const WeeklyDigestController = () => {
   const getPodcastByCreatorsThisWeek = async () => {
+    const dateToday = moment()
+      .tz("America/Los_Angeles")
+      .startOf("day")
+      .utc()
+      .format("YYYY-MM-DD");
+
+    const dateSevenDaysFromDateToday = moment()
+      .tz("America/Los_Angeles")
+      .startOf("day")
+      .utc()
+      .subtract(6, "day")
+      .format("YYYY-MM-DD");
+
+    try {
+      const podcasts = await Podcast.findAll({
+        where: {
+          channel: {
+            [Op.ne]: null,
+          },
+          [Op.and]: [
+            {
+              dateEpisode: {
+                [Op.lte]: dateToday,
+              },
+            },
+            {
+              dateEpisode: {
+                [Op.gte]: dateSevenDaysFromDateToday,
+              },
+            },
+          ],
+        },
+        raw: true,
+      });
+
+      return podcasts;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const getResourcesByCreatorsThisWeek = async () => {
     try {
       const podcasts = await Podcast.findAll({
         where: {
@@ -23,10 +67,6 @@ const WeeklyDigestController = () => {
       return [];
     }
   };
-
-  const getResourcesByCreatorsThisWeek = async () => {
-    
-  }
 
   return {
     getPodcastByCreatorsThisWeek,
