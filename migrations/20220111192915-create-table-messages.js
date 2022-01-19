467e9ddb476871a8b593dfd37b6871ea276e0c09
Douglas Perez <douglas.eduardo2000@gmail.com>
2022-01-19 14:53:28 -0400

    load messages

--- migrations/20220111192915-create-table-messages.js
+++ migrations/20220111192915-create-table-messages.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
 
-      ConversartionId: {
+      ConversationId: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
