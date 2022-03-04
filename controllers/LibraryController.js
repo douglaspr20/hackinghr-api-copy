02977ecaab75779f1c470f06b508a357347ca47d
Lourence Linao <lourence@Lourences-MacBook-Air.local>
2022-03-04 19:49:17 +0800

    [ADJUSTMENT][LOURENCE] - Learning Library

--- controllers/LibraryController.js
+++ controllers/LibraryController.js
@@ -102,7 +102,7 @@ const LibraryController = () => {
 
         const mailOptions = {
           from: process.env.SEND_IN_BLUE_SMTP_SENDER,
-          to: "enrique@hackinghr.io",
+          to: "lourencelinao13@gmail.com",
           subject: LabEmails.NEW_LIBRARY_CONTENT_FOR_APPROVAL.subject(),
           html: LabEmails.NEW_LIBRARY_CONTENT_FOR_APPROVAL.body(
             newLibrary.title,
