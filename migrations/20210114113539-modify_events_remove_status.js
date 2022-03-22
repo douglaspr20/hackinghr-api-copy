eca9fe3653ad9e829b7b1e0e97199c59f58610ce
Betsy Moreno <morenoelba2002@gmail.com>
2022-03-21 22:24:39 -0400

    Revert "log event's days"
    This reverts commit 7f60511400b74a22c7db25d28de629f66ca5f340.

---
+++ migrations/20210114113539-modify_events_remove_status.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn("Events", "status", {
+        type: Sequelize.JSON,
+        defaultValue: {},
+      }),
+      queryInterface.addColumn("Events", "users", {
+        type: Sequelize.ARRAY(Sequelize.INTEGER),
+        defaultValue: [],
+      }),
+    ]); 
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
