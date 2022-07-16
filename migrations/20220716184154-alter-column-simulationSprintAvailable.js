77f01672ccd5f15891f717b9baa777fcb2cca734
Douglas Perez <douglas.eduardo2000@gmail.com>
2022-07-16 14:48:13 -0400

    new migration

---
+++ migrations/20220716184154-alter-column-simulationSprintAvailable.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn("Users", "simulationSprintsAvailable", {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+      allowNull: false,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
